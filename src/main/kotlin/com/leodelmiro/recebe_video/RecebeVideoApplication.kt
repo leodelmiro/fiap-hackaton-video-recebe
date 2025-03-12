f03f6224c0078ca20ea64c3a4e0b618bb8f35891
Leonardo Delmiro <leodelmiroms@gmail.com>
2025-03-12 13:21:43 -0300

    feat: initial commit

---
+++ src/main/kotlin/com/leodelmiro/recebe_video/RecebeVideoApplication.kt
@@ -0,0 +1,11 @@
+package com.leodelmiro.recebe_video
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RecebeVideoApplication
+
+fun main(args: Array<String>) {
+	runApplication<RecebeVideoApplication>(*args)
+}
---
+++ src/test/kotlin/com/leodelmiro/recebe_video/RecebeVideoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.leodelmiro.recebe_video
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RecebeVideoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
